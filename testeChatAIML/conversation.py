7f52a3cad6f7a81a0aeab1ca1edb47fa9950692a
Audrey <aerv@ic.ufal.br>
2019-12-05 15:36:06 -0300

    rodando bot

---
+++ testeChatAIML/conversation.py
@@ -0,0 +1,30 @@
+import os
+import aiml
+
+BRAIN_FILE = "brain.dump"
+
+k = aiml.Kernel()
+
+if os.path.exists(BRAIN_FILE):
+    print("Loading from brain file: " + BRAIN_FILE)
+else:
+    k.bootstrap(learnFiles="learningFileList.aiml", commands="LEARN AIML")
+    print("Saving brain file: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
+
+    while True:
+        input_text = input("USER > ")
+        response = k.respond(input_text)
+        print("BOT > ", response)
+
+'''
+k.learn("learningFileList.aiml")
+k.respond("LEARN AIML")
+
+while True:
+    reply = k.respond(input("User > "))
+    if reply:
+        print("bot > ", reply)
+    else:
+        print("bot > :", )
+'''
