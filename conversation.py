61404289404f2377769859e63c58559ca305db2d
Audrey <aerv@ic.ufal.br>
2019-12-06 21:51:35 -0300

    rodando bot

--- conversation.py
+++ conversation.py
@@ -17,14 +17,16 @@ else:
         response = k.respond(input_text)
         print("BOT > ", response)
 
+
 '''
+k = aiml.Kernel()
 k.learn("learningFileList.aiml")
 k.respond("LEARN AIML")
 
 while True:
     reply = k.respond(input("User > "))
     if reply:
-        print("bot > ", reply)
+        print("Bot > ", reply)
     else:
-        print("bot > :", )
+        print("Bot > :", )
 '''
