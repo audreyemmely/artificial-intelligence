b99c8b15804ca314f1d49b7cc9d53b6bf2ef6a50
Audrey <aerv@ic.ufal.br>
2019-12-12 10:17:23 -0300

    executando o chatbot

---
+++ LISTA1_questão2/conversation.py
@@ -0,0 +1,32 @@
+import os
+import aiml
+
+BRAIN_FILE = "brain.dump" #executar o programa dentro da pasta "LISTA1_questão2" para poder funcionar corretamente
+
+k = aiml.Kernel()
+
+if os.path.exists(BRAIN_FILE):
+    print("Loading from brain file: " + BRAIN_FILE)
+else:
+    k.bootstrap(learnFiles="learningFileList.aiml", commands="LEARN AIML")
+    print("Saving brain file: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
+
+    while True:
+        input_text = input("USER > ")
+        response = k.respond(input_text)
+        print("BOT > ", response)
+
+
+'''
+k = aiml.Kernel()
+k.learn("learningFileList.aiml")
+k.respond("LEARN AIML")
+
+while True:
+    reply = k.respond(input("User > "))
+    if reply:
+        print("Bot > ", reply)
+    else:
+        print("Bot > :", )
+'''
